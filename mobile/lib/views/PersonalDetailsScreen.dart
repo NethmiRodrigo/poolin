ff6dc46959142691c9370a22cb3d16697e4c3163
Dulaj <shad3s.of.m3@gmail.com>
2022-07-11 09:03:03 +0530

    add validation to personal details screen

--- mobile/lib/views/PersonalDetailsScreen.dart
+++ mobile/lib/views/PersonalDetailsScreen.dart
@@ -1,80 +1,113 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mobile/utils/widget_functions.dart';
 import '../theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Form Validation Demo';
+
     return MaterialApp(
-      title: 'Personal Details Screen',
+      title: appTitle,
       theme: AppTheme().themeData,
-      home: const RegisterScreen(),
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: const MyCustomForm(),
+      ),
     );
   }
 }
 
-class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
 
   @override
-  State<RegisterScreen> createState() => _RegisterScreenState();
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _pass = TextEditingController();
+  final TextEditingController _confirmPass = TextEditingController();
   List<bool> isSelected = [false, true];
+
+  @override
+  void dispose() {
+    _pass.dispose();
+    _confirmPass.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-
     const double padding = 16;
     const sidePadding = EdgeInsets.symmetric(horizontal: padding);
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: Padding(
-            padding: sidePadding,
+    // Build a Form widget using the _formKey created above.
+    return Padding(
+      padding: sidePadding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          addVerticalSpace(48),
+          Align(
+            alignment: Alignment.topRight,
+            child: Text(
+              'STEP 5/5',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          Image.asset('assets/images/logo.png', scale: 3),
+          Text(
+            "You're almost there!",
+            style: Theme.of(context)
+                .textTheme
+                .headline3!
+                .merge(TextStyle(color: Colors.black)),
+            textAlign: TextAlign.left,
+          ),
+          addVerticalSpace(48),
+          Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                addVerticalSpace(48),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Text(
-                    'STEP 5/5',
-                    style: Theme.of(context).textTheme.subtitle1,
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'First Name',
+                    isDense: true,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 16),
                   ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field cannot be empty';
+                    } else if (value.length > 20) {
+                      return 'Name length cannot be more than 20 characters';
+                    }
+
+                    return null;
+                  },
                 ),
-                Image.asset('assets/images/logo.png', scale: 3),
-                Text(
-                  "You're almost there!",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3!
-                      .merge(TextStyle(color: Colors.black)),
-                  textAlign: TextAlign.left,
-                ),
-                addVerticalSpace(48),
-                const TextField(
-                    decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'First Name',
-                  isDense: true,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                )),
                 addVerticalSpace(24),
-                const TextField(
-                  obscureText: true,
+                TextFormField(
+                  controller: _pass,
                   decoration: InputDecoration(
                     isDense: true,
                     border: OutlineInputBorder(),
@@ -82,6 +115,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     contentPadding:
                         EdgeInsets.symmetric(vertical: 10, horizontal: 16),
                   ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field cannot be empty';
+                    } else if (value.length > 20) {
+                      return 'Name length cannot be more than 20 characters';
+                    }
+                    return null;
+                  },
                 ),
                 addVerticalSpace(24),
                 ToggleButtons(
@@ -130,13 +172,22 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       backgroundColor: Colors.black,
                       minimumSize: const Size.fromHeight(50),
                       textStyle: Theme.of(context).textTheme.bodyText1),
-                  onPressed: () {},
                   child: const Text('Start Pooling!'),
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
