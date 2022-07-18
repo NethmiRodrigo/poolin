b93f5ef86b4c08e9d67225f5ac59c68e0a7988ad
npathirana1 <nethmi.pathirana@gmail.com>
2022-07-18 10:15:47 +0530

    Send OTP to update mobile

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,9 +2,15 @@ import { Request, Response, Router } from "express";
 import { isEmpty } from "class-validator";
 import bcrypt from "bcrypt";
 
+/** Entities */
 import { User } from "../database/entity/User";
+
+/** Utility functions */
 import { AppError } from "../util/error-handler";
+
+/** Middleware */
 import auth from "../middleware/auth";
+//import { getLoggedInUser } from "../routes/auth/login";
 
 const updateInfo = async (req: Request, res: Response) => {
   const user: User = res.locals.user;
@@ -63,9 +69,39 @@ const updatePassword = async (req: Request, res: Response) => {
   return res.status(200).json({ success: "Password successfully updated" });
 };
 
+const updateMobile = async (req: Request, res: Response) => {
+  let {mobile} = req.body;
+  let errors: any = {};
+
+  if (isEmpty(mobile)) errors.mobile = "Please enter mobile number";
+
+  if (Object.keys(errors).length > 0) throw new AppError(401, errors);
+
+  // remove all non-numeric characters except '+'
+  mobile = mobile.replace(/[^\+0-9]/gi, "");
+
+  // check if mobile number is valid
+  // (should have a leading '+' followed by 11 digits)
+  if (!/^\+[0-9]+$/.test(mobile) || !(mobile.length == 12))
+    throw new AppError(401, {}, "Invalid mobile number");
+
+  // check if mobile number already registered
+  const user = await User.findOneBy({ mobile });
+  if (user) throw new AppError(401, { error: "Mobile already registered" });
+
+  // const { result, otp } = await smsOTPAtSignup(mobile);
+  // if (!result)
+  //   throw new AppError(400, {}, "Couldn't send OTP. Please try again");
+
+  // return res.status(200).json({ success: "OTP sent via SMS", otp });
+
+}
+
 const router = Router();
 
+//router.get("/me", auth, getLoggedInUser);
 router.post("/updateInfo", auth, updateInfo);
 router.post("/updatePassword", auth, updatePassword);
+router.post("/updateMobile", auth, updateMobile);
 
 export default router;
