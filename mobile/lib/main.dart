8c9c6a915cf47431fd4030258277b9acf77d288f
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-07-20 13:31:57 +0530

    Removed unnecessary constants file

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/login/login_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile/screens/register/register_screen.dart';
 import './theme.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   await dotenv.load();
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Poolin',
       theme: AppTheme().themeData,
-      home: const LoginScreen(),
+      home: const RegisterScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- mobile/lib/services/constants.dart
+++
@@ -1,5 +0,0 @@
-const header = {
-  "Content-Type": "application/json",
-  "accept": "application/json",
-  "Access-Control-Allow-Origin": "*"
-};
