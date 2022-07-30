83392af4754f47a72ed942aa1c7f6da6d05b8c24
Dulaj <shad3s.of.m3@gmail.com>
2022-07-30 23:20:57 +0530

    complete ride offer service

--- mobile/lib/services/ride_offer_service.dart
+++ mobile/lib/services/ride_offer_service.dart
@@ -6,20 +6,32 @@ import 'package:mobile/models/coordinate_model.dart';
 
 import '../constants/constants.dart' as constants;
 
-final baseURL = '${dotenv.env['API_URL']}/api/ride/';
+final baseURL = '${dotenv.env['API_URL']}/api/ride';
+
+void prettyPrintJson(String input) {
+  const JsonDecoder decoder = JsonDecoder();
+  const JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  final dynamic object = decoder.convert(input);
+  final dynamic prettyString = encoder.convert(object);
+  prettyString.split('\n').forEach((dynamic element) => print(element));
+}
 
 Future<http.Response> postOffer(RideOffer rideOffer) async {
   Map data = {
-    'src': coordinateToJson(rideOffer.source),
-    'dest': coordinateToJson(rideOffer.destination),
+    'src': rideOffer.source,
+    'dest': rideOffer.destination,
     'ppkm': rideOffer.ppkm,
+    'distance': rideOffer.distance,
     'seats': rideOffer.seatCount,
-    'startTime': rideOffer.startTime,
-    'endTime': rideOffer.startTime.add(Duration(minutes: rideOffer.duration)),
+    'startTime': rideOffer.startTime.toString(),
+    'endTime': rideOffer.startTime
+        .add(Duration(minutes: rideOffer.duration))
+        .toString(),
     'userId': 1,
   };
 
   String body = json.encode(data);
+  prettyPrintJson(body);
   var url = Uri.parse('$baseURL/create-offer');
   var response = await http.post(
     url,
