0a9bfb6aa2a08b010d70e75cce7cec1c4143a797
azeimtiaz <imtiaz.azma@gmail.com>
2022-07-30 11:34:55 +0530

    set home screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:mobile/screens/register/register_screen.dart';
+import 'package:mobile/screens/rides/ride_history.dart';
 
 import './theme.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Poolin',
       theme: AppTheme().themeData,
-      home: const RegisterScreen(),
+      home: const RideHistory(),
       debugShowCheckedModeBanner: false,
     );
   }
