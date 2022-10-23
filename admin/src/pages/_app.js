bbc08f5dafd28bcf39a092914fc24dda7eefb41d
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-10-23 17:34:51 +0530

    Cleanup login call and add toast

--- admin/src/pages/_app.js
+++ admin/src/pages/_app.js
@@ -1,11 +1,14 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { createEmotionCache } from '../utils/create-emotion-cache';
-import { theme } from '../theme';
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { createEmotionCache } from "../utils/create-emotion-cache";
+import { theme } from "../theme";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -17,16 +20,12 @@ const App = (props) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>
-          Material Kit Pro
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
+        <title>Poolin</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <ThemeProvider theme={theme}>
+          <ToastContainer />
           <CssBaseline />
           {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
