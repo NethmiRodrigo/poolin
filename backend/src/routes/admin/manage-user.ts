4abab55dc0be67b44773733c76539cd01f598a10
npathirana1 <nethmi.pathirana@gmail.com>
2022-10-24 11:28:42 +0530

    get all users

--- backend/src/routes/admin/manage-user.ts
+++ backend/src/routes/admin/manage-user.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
+import { Polygon } from "wkx";
 
 /** Entities */
 import { User } from "../../database/entity/User";
 
 /** Utility functions */
 import { AppError } from "../../util/error-handler";
+
+import { AppDataSource } from "../../data-source";
 
 /**
  * Verify user
@@ -22,4 +25,20 @@ export const toggleVerification = async (req: Request, res: Response) => {
   await user.save();
 
   return res.json({ user });
+};
+
+
+
+
+/**
+ * 
+ * Fetch all users
+ */
+export const fetchAllUsers = async (req: Request, res: Response) => {
+  // const { id, verified } = req.params;
+
+  const userRepository = await AppDataSource.getRepository(User);
+  const allUsers = await userRepository.find();
+  console.log("All users: ", allUsers);
+  return res.json({allUsers})
 };
