42254c37cda3283e4084f9a9ca084de7e12c09e9
npathirana1 <nethmi.pathirana@gmail.com>
2022-10-25 20:41:25 +0530

    Commit

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -20,7 +20,7 @@ const Dashboard = () => {
   useEffect(async () => {
     try {
       const response = await getAllsales();
-      console.log(response)
+      console.log(response);
       setSales(response);
     } catch (error) {
       toast.error(error.message, {
@@ -39,48 +39,49 @@ const Dashboard = () => {
     }
   }, []);
   return (
-  <>
-    <Head>
-      <title>Dashboard | Poolin</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8,
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid container spacing={3}>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Budget />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalCustomers />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TasksProgress />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalProfit sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
+    <>
+      <Head>
+        <title>Dashboard | Poolin</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <Budget />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TotalCustomers />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TasksProgress />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TotalProfit sx={{ height: "100%" }} />
+            </Grid>
+            {/* <Grid item lg={8} md={12} xl={9} xs={12}>
             <Sales sales={sales} />
+          </Grid> */}
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <TrafficByDevice sx={{ height: "100%" }} />
+            </Grid>
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <LatestProducts sx={{ height: "100%" }} />
+            </Grid>
+            <Grid item lg={8} md={12} xl={9} xs={12}>
+              <LatestOrders />
+            </Grid>
           </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <TrafficByDevice sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <LatestProducts sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <LatestOrders />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-)};
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
