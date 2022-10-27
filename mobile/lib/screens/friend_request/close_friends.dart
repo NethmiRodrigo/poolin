032e65ed48d99c6180a4bb96d86d8f926508ae76
Imesh <2019cs171@stu.ucsc.lk>
2022-10-27 16:35:42 +0530

    close friends ui

---
+++ mobile/lib/screens/friend_request/close_friends.dart
@@ -0,0 +1,88 @@
+import 'package:poolin/models/mutual_friends.dart';
+import 'package:mobile/screens/user/profile/user_profile_screen.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile/custom/wide_button.dart';
+import 'package:mobile/custom/outline_button.dart';
+import 'package:mobile/fonts.dart';
+import 'package:mobile/utils/widget_functions.dart';
+import '../../../colors.dart';
+import 'package:akar_icons_flutter/akar_icons_flutter.dart';
+
+class CloseFriendsScreen extends StatefulWidget {
+  const CloseFriendsScreen({super.key});
+
+  @override
+  CloseFriendsScreenState createState() {
+    return CloseFriendsScreenState();
+  }
+}
+
+class CloseFriendsScreenState extends State<CloseFriendsScreen> {
+  static List<mutualfriendModel> main_friends_list = [
+    mutualfriendModel(
+        "Dulaj Prabash", "Lecture", "assets/images/man.jpeg", "See more"),
+    mutualfriendModel("Nimeshi karunarathne", "Students",
+        "assets/images/woman.jpeg", "See more"),
+    mutualfriendModel(
+        "Deshan alpitiya", "Students", "assets/images/user.jpg", "See more"),
+    mutualfriendModel(
+        "Shershi gomitri", "Students", "assets/images/woman.jpeg", "See more"),
+  ];
+
+  List<mutualfriendModel> display_list = List.from(main_friends_list);
+
+  void updatelist(String value) {
+    setState(() {
+      display_list = main_friends_list
+          .where((element) =>
+              element.Name!.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    const double padding = 16;
+    const sidePadding = EdgeInsets.symmetric(horizontal: padding);
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  
+                },
+              ),
+              Text(
+                "Close friends",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              //////////////////////////////////////////////////
+              
+              SizedBox(
+                height: 20,
+              ),
+              ///////////////////////////////////////////////////////////////
+              
+            ]),
+      ),
+    );
+  }
+}
