2f56ac4b6d14b868b800e69870c460a1f03d98f1
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-10-27 20:07:08 +0530

    Cleaned up admin

--- admin/src/pages/payment1.js
+++ admin/src/pages/payment1.js
@@ -1,20 +1,14 @@
-import Head from 'next/head';
-import { Box, Container, Grid } from '@mui/material';
-import { Budget } from '../components/dashboard/budget';
-import { TotalIncome } from '../components/payment/total-income';
-import { TotalPayables } from '../components/payment/total-payables';
-import { TotalProfit } from '../components/payment/total-profit';
-import { TotalCustomers } from '../components/payment/total-customers';
-import { PaymentListResults } from '../components/payment/payment-list-results';
+import Head from "next/head";
+import { Box, Container, Grid } from "@mui/material";
+import { TotalIncome } from "../components/payment/total-income";
+import { TotalPayables } from "../components/payment/total-payables";
+import { TotalProfit } from "../components/payment/total-profit";
+import { TotalCustomers } from "../components/payment/total-customers";
+import { PaymentListResults } from "../components/payment/payment-list-results";
 
-import { LatestOrders } from '../components/dashboard/latest-orders';
-import { LatestProducts } from '../components/dashboard/latest-products';
-import { Sales } from '../components/dashboard/sales';
-import { TasksProgress } from '../components/dashboard/tasks-progress';
-import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
-import { DashboardLayout } from '../components/dashboard-layout';
+import { DashboardLayout } from "../components/dashboard-layout";
 
-import { getAllPayment, getAllPayments } from 'src/services/payments.service';
+import { getAllPayments } from "src/services/payments.service";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
@@ -26,7 +20,6 @@ const Payment = () => {
   useEffect(async () => {
     try {
       const response = await getAllPayments();
-      // console.log(response.allPayments[0])
       setPayment(response);
     } catch (error) {
       toast.error(error.message, {
@@ -46,78 +39,41 @@ const Payment = () => {
   }, []);
 
   return (
-  <>
-    <Head>
-      <title>
-        Payments
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalIncome />
+    <>
+      <Head>
+        <title>Payments</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <TotalIncome />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TotalPayables />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TotalProfit />
+            </Grid>
+            <Grid item xl={3} lg={3} sm={6} xs={12}>
+              <TotalCustomers />
+            </Grid>
+            <Grid item md={12} xl={9} xs={12}>
+              <PaymentListResults payment={payment} />
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalPayables />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalProfit />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <PaymentListResults payment={payment}/>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-)};
+        </Container>
+      </Box>
+    </>
+  );
+};
 
-Payment.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Payment.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Payment;
