42c09512587c9f30c56aaf477106500894250e9f
npathirana1 <nethmi.pathirana@gmail.com>
2022-10-27 19:12:18 +0530

    User entity

--- backend/src/database/entity/User.ts
+++ backend/src/database/entity/User.ts
@@ -73,7 +73,7 @@ export class User extends BaseEntity {
   })
   gender: Gender;
 
-  @Column({ default: 'https://i.ibb.co/qgVMXFS/profile-icon-9.png' })
+  @Column({ default: 'https://i.ibb.co/qgVMXFS/profile-icon-9.png', nullable: true })
   @Exclude()
   profileImageUri: string;
 
