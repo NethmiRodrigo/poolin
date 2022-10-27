7e9d4f08ad92a11c66804fb521d6c31aedfc53af
Imesh <2019cs171@stu.ucsc.lk>
2022-10-27 16:30:16 +0530

    add mutual friends model

---
+++ mobile/lib/models/mutual_friends.dart
@@ -0,0 +1,8 @@
+class mutualfriendModel {
+  String? Name;
+  String? position;
+  String? friendimageurl;
+  String? Seemore;
+
+  mutualfriendModel(this.Name, this.position, this.friendimageurl, this.Seemore);
+}
