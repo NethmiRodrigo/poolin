11042c95e2d5af8dc844a5ea22d8369c9c7510de
NethmiRodrigo <neth23rodrigo@gmail.com>
2022-10-27 19:40:36 +0530

    Removed unused imports

--- backend/src/database/entity/Payment.ts
+++ backend/src/database/entity/Payment.ts
@@ -1,53 +1,47 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    BaseEntity,
-    Index,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ManyToOne,
-  } from "typeorm";
-  import { Exclude } from "class-transformer";
-  import { User } from "./User";
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  ManyToOne,
+} from "typeorm";
+import { Exclude } from "class-transformer";
+import { User } from "./User";
 import { RideRequest } from "./RideRequest";
-  
-  
-  @Entity()
-  export class Payment extends BaseEntity {
-    constructor(payment?: Partial<Payment>) {
-      super();
-      Object.assign(this, payment);
-    }
-  
-    @Exclude()
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    driverID: number;
-  
-    @Column()
-    numberOfPassengers: number;
 
-    @Column()
-    totalIncome: number;
-  
-    @Column()
-    totalPayable: number;
-
-    @Column()
-    totalProfit: number;
-
-    @ManyToOne(() => RideRequest, (RideRequest) => RideRequest.id, {
-      cascade: ["remove"],
-      onDelete: "CASCADE",
-    })
-    owner: User;
-  
-  
-    @CreateDateColumn()
-    paymentDate: Date;
-  
+@Entity()
+export class Payment extends BaseEntity {
+  constructor(payment?: Partial<Payment>) {
+    super();
+    Object.assign(this, payment);
   }
-  
+
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  driverID: number;
+
+  @Column()
+  numberOfPassengers: number;
+
+  @Column()
+  totalIncome: number;
+
+  @Column()
+  totalPayable: number;
+
+  @Column()
+  totalProfit: number;
+
+  @ManyToOne(() => RideRequest, (RideRequest) => RideRequest.id, {
+    cascade: ["remove"],
+    onDelete: "CASCADE",
+  })
+  owner: User;
+
+  @CreateDateColumn()
+  paymentDate: Date;
+}
